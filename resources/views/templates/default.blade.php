bb235b2eda452ce36b2e15609c7d3aa70d0bbb7c
retro12 <goranradmanovic@gmail.com>
2017-01-19 21:47:36 +0100

    Incuding footer in default pug template and compailing it to blade template

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -62,6 +62,14 @@
             </div>
           </nav>
         </menu>@yield('content')
+        <footer class="footer">
+          <div class="col-md-12">
+            <div class="col-md-4">
+              <div class="footer__social"><span>Follow</span><a class="footer__social--link footer__social--link--fb" href="#"></a><a class="footer__social--link footer__social--link--tw" href="#"></a><a class="footer__social--link footer__social--link--in" href="#">Instagram</a><a class="footer__social--link footer__social--link--li" href="#">Linkedin</a></div>
+            </div>
+            <div class="col-md-8"></div>
+          </div>
+        </footer>
       </div>
     </div>
     <script src="js/app.js"></script>
