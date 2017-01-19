d4f5921417a689ac0f1cbf8c31486eedc677e457
retro12 <goranradmanovic@gmail.com>
2017-01-19 21:49:40 +0100

    Creating footer pug file and compailing to blade file

---
+++ resources/views/templates/partials/footer.blade.php
@@ -0,0 +1,9 @@
+
+<footer class="footer">
+  <div class="col-md-12">
+    <div class="col-md-4">
+      <div class="footer__social"><span>Follow</span><a class="footer__social--link footer__social--link--fb" href="#"></a><a class="footer__social--link footer__social--link--tw" href="#"></a><a class="footer__social--link footer__social--link--in" href="#">Instagram</a><a class="footer__social--link footer__social--link--li" href="#">Linkedin</a></div>
+    </div>
+    <div class="col-md-8"></div>
+  </div>
+</footer>
