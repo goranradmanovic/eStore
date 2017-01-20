53958215ae14bf695e126cc874bef1fff4dafb8a
retro12 <goranradmanovic@gmail.com>
2017-01-20 22:25:12 +0100

    Compailing pug files to blade ect. working on the footer for the website

--- resources/views/templates/partials/footer.blade.php
+++ resources/views/templates/partials/footer.blade.php
@@ -2,8 +2,14 @@
 <footer class="footer">
   <div class="col-md-12">
     <div class="col-md-4">
-      <div class="footer__social"><span>Follow</span><a class="footer__social--link footer__social--link--fb" href="#"></a><a class="footer__social--link footer__social--link--tw" href="#"></a><a class="footer__social--link footer__social--link--in" href="#">Instagram</a><a class="footer__social--link footer__social--link--li" href="#">Linkedin</a></div>
+      <div class="footer__text">Follow</div>
+      <div class="footer__social"><a class="footer__social--link footer__social--link--fb" href="#"></a><a class="footer__social--link footer__social--link--tw" href="#"></a><a class="footer__social--link footer__social--link--in" href="#"></a><a class="footer__social--link footer__social--link--li" href="#"></a></div>
     </div>
-    <div class="col-md-8"></div>
+    <div class="col-md-8">
+      <div class="footer__nav"><a class="footer__nav--link" href="#">Privacy</a><a class="footer__nav--link" href="#">Terms</a><a class="footer__nav--link" href="#">Disclaimer</a><a class="footer__nav--link" href="#">Site Map</a></div>
+      <div class="footer__nav--contact">
+        <h3>Concat Us</h3>
+      </div>
+    </div>
   </div>
 </footer>
