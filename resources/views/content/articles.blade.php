a18b0ba25b06ba9d9759d8fd1312a1ae7cd5f730
retro12 <goranradmanovic@gmail.com>
2017-01-24 22:22:33 +0100

    Compiled blade files

--- resources/views/content/articles.blade.php
+++ resources/views/content/articles.blade.php
@@ -1,18 +1,22 @@
 
-<div class="col-md-12">
-  <div class="articles">
-    <div class="articles__item" v-for="article in articles">
-      <h3 class="articles__item--title"><a class="articles_item--link" :href="article.titleLink" target="blank" v-text="article.title"></a></h3>
-      <div class="articles__item--image"><a :href="article.productLink" target="_blank"><img :src="article.imgURL" alt="Product image"/></a></div>
-      <div class="articles__item--details">
-        <p class="article__item--details--text" v-text="article.description"></p>
+<div class="row">
+  <div class="col-md-12">
+    <div class="articles" v-for="article in articles">
+      <div class="col-md-4">
+        <div class="articles__item">
+          <h3 class="articles__item--title"><a class="articles_item--title--link" :href="article.titleLink" target="blank" v-text="article.title"></a></h3>
+          <div class="articles__item--link"><a :href="article.productLink" target="_blank"><img class="articles__item--link--image" :src="article.imgURL" alt="Product image"/></a></div>
+          <div class="articles__item--details">
+            <p class="articles__item--details--text" v-text="article.description"></p>
+          </div>
+          <div class="articles__item--price">
+            @verbatim
+            $ {{ article.price }}
+            @endverbatim
+          </div>
+          <div class="articles__item--button"><a :href="article.buttonLink">It's a bargain</a></div>
+        </div>
       </div>
-      <div class="articles__item--price">
-        @verbatim
-        $ {{ article.price }}
-        @endverbatim
-      </div>
-      <div class="articles__item--button"><a :href="article.buttonLink">Click this</a></div>
     </div>
   </div>
 </div>
--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -15,54 +15,53 @@
     <title>@yield('title')</title>
   </head>
   <body>
-    <div class="container-fluid" id="app">
+    <menu class="main__menu">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="#"><span class="brand__img"></span></a>
+          </div>
+          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li v-on:click="active"><a href="#">Matcha Tea<span class="sr-only">(current)</span></a></li>
+              <li><a href="#">Fitness Gear</a></li>
+              <li><a href="#">Food Supplements</a></li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider" role="separator"></li>
+                  <li><a href="#">Separated link</a></li>
+                  <li class="divider" role="separator"></li>
+                  <li><a href="#">One more separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+            <form class="navbar-form navbar-left">
+              <div class="form-group">
+                <input class="form-control" type="text" placeholder="Search">
+              </div>
+              <button class="btn btn-default" type="submit">Search</button>
+            </form>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="#">Link</a></li>
+              <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Action</a></li>
+                  <li><a href="#">Another action</a></li>
+                  <li><a href="#">Something else here</a></li>
+                  <li class="divider" role="separator"></li>
+                  <li><a href="#">Separated link</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </menu>
+    <div class="container-fluid" id="app">@yield('content')
       <div class="row">
-        <menu class="main__menu">
-          <nav class="navbar navbar-default">
-            <div class="container-fluid">
-              <div class="navbar-header">
-                <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false"><span class="sr-only">Toggle navigation</span><span class="icon-bar"></span><span class="icon-bar"></span><span class="icon-bar"></span></button><a class="navbar-brand" href="#"><span class="brand__img"></span></a>
-              </div>
-              <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
-                  <li v-on:click="active"><a href="#">Matcha Tea<span class="sr-only">(current)</span></a></li>
-                  <li><a href="#">Fitness Gear</a></li>
-                  <li><a href="#">Food Supplements</a></li>
-                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li class="divider" role="separator"></li>
-                      <li><a href="#">Separated link</a></li>
-                      <li class="divider" role="separator"></li>
-                      <li><a href="#">One more separated link</a></li>
-                    </ul>
-                  </li>
-                </ul>
-                <form class="navbar-form navbar-left">
-                  <div class="form-group">
-                    <input class="form-control" type="text" placeholder="Search">
-                  </div>
-                  <button class="btn btn-default" type="submit">Search</button>
-                </form>
-                <ul class="nav navbar-nav navbar-right">
-                  <li><a href="#">Link</a></li>
-                  <li class="dropdown"><a class="dropdown-toggle" href="#" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
-                    <ul class="dropdown-menu">
-                      <li><a href="#">Action</a></li>
-                      <li><a href="#">Another action</a></li>
-                      <li><a href="#">Something else here</a></li>
-                      <li class="divider" role="separator"></li>
-                      <li><a href="#">Separated link</a></li>
-                    </ul>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-        </menu>
-        <div class="container main-container">@yield('content')</div>
         <footer class="footer">
           <div class="col-md-12">
             <div class="col-md-4">
