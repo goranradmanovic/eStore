01bb586934127ac79da397a3af1ce21e3ff88724
retro12 <goranradmanovic@gmail.com>
2017-01-26 22:14:10 +0100

    Adding new ScrolToTop function

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -106,3 +106,27 @@ const app = new Vue({
 		articles: articles,
 	},
 });
+
+//jQuery
+$(document).ready(function(){
+	//Calling the function
+	scrollToTop();
+	
+});
+
+function scrollToTop() {
+	//Check to see if the window is top if not then display button
+	$(window).scroll(function(){
+		if ($(this).scrollTop() > 100) {
+			$('.scrollToTop').fadeIn();
+		} else {
+			$('.scrollToTop').fadeOut();
+		}
+	});
+	
+	//Click event to scroll to top
+	$('.scrollToTop').click(function(){
+		$('html, body').animate({scrollTop : 0}, 800);
+		return false;
+	});
+}
