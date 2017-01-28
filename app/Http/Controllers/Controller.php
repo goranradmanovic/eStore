c5b4492d217bb9cf678ec28e9843fb01860a6325
retro12 <goranradmanovic@gmail.com>
2017-01-28 22:32:42 +0100

    Creating methods for returning home page

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,7 +7,20 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+use App\Http\Requests;
+use Illuminate\Http\Request;
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //Method for returning home view ect. page
+    public function index() {
+    	return view('home');
+    }
+
+    //Getting data from the Subscribe form and writing data to the database
+    public function createSubscription() {
+
+    }
 }
