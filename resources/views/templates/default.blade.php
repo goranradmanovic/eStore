4725d5837e9fc9672d9dd138414ca41f3c05d737
retro12 <goranradmanovic@gmail.com>
2017-01-29 21:48:38 +0100

    Cleaning some code

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -89,6 +89,5 @@
       </div>
     </div>
   </body>
-  <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
   <script src="js/app.js"></script>
 </html>
