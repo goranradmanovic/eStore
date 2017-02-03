bc5def783c75d53e4787dacfa78d3c3daa0de3ef
retro12 <goranradmanovic@gmail.com>
2017-02-03 22:56:56 +0100

    Checking if validation rules fails and sending server response to the client

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,66 +2,62 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Routing\Controller as BaseController;
-use Illuminate\Foundation\Validation\ValidatesRequests;
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-
-use Illuminate\Http\Request;
+use Validator;
+use Illuminate\{
+    Foundation\Bus\DispatchesJobs,
+    Routing\Controller as BaseController,
+    Foundation\Validation\ValidatesRequests,
+    Foundation\Auth\Access\AuthorizesRequests,
+    Http\Request
+};
 use App\Models\Subscriber\Subscriber;
 
 class Controller extends BaseController
 {
+    //This is traits
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
+   
     //Method for returning home view ect. page
-    public function index() {
-    	return view('home');
+    public function index()
+    {
+    	return view('home'); //Return home page
     }
 
-    //Getting data from the Subscribe form and writing data to the database
-    public function createSubscription(Request $request) {
+    //Getting data from the Subscribe form and writing data to the database , 
+    public function createSubscription(Request $request)
+    {
+        //Validate user form input ect. user email (Koristimo $this->validate() m. iz traita ValidatesRequests,da nije trait ne bi se ovako mogao korsititi). validate() m. automaticly send json response to the client
+    	$validator = Validator::make($request->all(), [
+    		'subscribeEmail' => 'required|email|unique:subscribers,email|max:255',
+    	])->validate();
 
-        //Validate user form input ect. user email
-    	$this->validate($request, [
-    		'subscribeEmail' => 'required|email|unique:subscribers,email|max:255'
-    	]);
-
-        //If form fields validation fails
-        if () {
-
-        }
-
-
-        //Creating new entry to the Subscriber table
-        Subscriber::create(['email' => $request->input('subscribeEmail')]);
-
-    	/*if (Response::ajax()) {
-            //return response()->json(['responseText' => 'OK'], 200);
-            
-    	}*/
-
-      
-           /* if($request->ajax())
+        //Checking if validation rules fails
+        if ($validator->fails())
+        {
+            //Checking if request is ajax
+            if($request->ajax())
             {
+                //Return response with 
                 return response()->json(array(
                     'success' => false,
                     'message' => 'There are incorect values in the form!',
                     'errors' => $validator->getMessageBag()->toArray()
                 ), 422);
-            }*/
+            }
+            else
+            {
+                //Fallback if call is not ajax call
+                return redirect('/')->withErrors($validator)->withInput($request->except('email'));
+            }
+        }
 
-            /*return response()->json(array(
-                    'success' => false,
-                    'message' => 'There are incorect values in the form!',
-                    'errors' => $validator->getMessageBag()->toArray()
-                ), 422);*/
+        //Creating new entry to the Subscriber table
+        Subscriber::create(['email' => $request->input('subscribeEmail')]);
 
-        //return response()->view('hello', $data, 200);
-       
+        //Sending email message to the user subscriber email
+        //Implementing email service
 
     	//Return message status if everything is OK
-    	return response()->json(['responseText' => 'Success!'], 200);
-    	//return response()->json(['responseText' => $request->all()], 200);
+    	return response()->json(['responseText' => 'You are successfuly subscribed.'], 200);
     }
 }
