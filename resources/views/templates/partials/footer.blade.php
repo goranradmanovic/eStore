21e99a09a3f951a5ee37dc53ce563b5690bf3922
retro12 <goranradmanovic@gmail.com>
2017-02-08 22:18:08 +0100

    Adding VueJs submit attr

--- resources/views/templates/partials/footer.blade.php
+++ resources/views/templates/partials/footer.blade.php
@@ -10,7 +10,7 @@
         <div class="footer__nav"><a class="footer__nav--link" href="#">Privacy</a><a class="footer__nav--link" href="#">Terms</a><a class="footer__nav--link" href="#">Disclaimer</a><a class="footer__nav--link" href="#">Site Map</a><a class="footer__nav--link" href="#">Contac Us</a></div>
         <div class="footer__form">
           <h3 class="footer__form--title">Subscribe</h3>
-          <form class="form-inline footer__form--inline" action="{{ url('/') }}" method="post" autocomplete="off" id="subscribeForm"> 
+          <form class="form-inline footer__form--inline" action="{{ url('/') }}" method="post" autocomplete="off" id="subscribeForm" v-on:submit="submitSubscribeEmail"> 
             <div class="form-group">
               <input class="form-control footer__form--email" type="email" name="subscribeEmail" placeholder="Email Address" v-model="email" value="{{ old('email') }}"/>
               <input class="footer__form--btn" type="submit" value="Submit"/>{{ csrf_field() }}
