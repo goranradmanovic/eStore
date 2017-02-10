9dbef1e269bef6395d08d6cfb57fdd5be44ac79a
retro12 <goranradmanovic@gmail.com>
2017-02-10 22:54:21 +0100

    Setting up mailgun email services

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.com'),
 
     /*
     |--------------------------------------------------------------------------
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'milosdakic89@gmail.com'),
         'name' => env('MAIL_FROM_NAME', 'PiggyBank'),
     ],
 
