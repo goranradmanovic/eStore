6070fb1241ab6f617818844485e911fdeb2ef2af
retro12 <goranradmanovic@gmail.com>
2017-02-16 22:10:12 +0100

    Changing the example article to product items from the database

--- resources/views/content/articles.blade.php
+++ resources/views/content/articles.blade.php
@@ -1,20 +1,18 @@
 
 <div class="row">
-  <p>@{{productItems}}</p>
-  <p>@{{year}}</p>
   <div class="col-md-12">
-    <div class="articles" v-for="article in articles">
+    <div class="articles" v-for="product in products">
       <div class="col-md-4">
         <div class="articles__item">
-          <h3 class="articles__item--title"><a class="articles_item--title--link" :href="article.titleLink" target="blank" v-text="article.title"></a></h3>
-          <div class="articles__item--link"><a :href="article.productLink" target="_blank"><img class="articles__item--link--image" :src="article.imgURL" alt="Product image"/></a></div>
+          <h3 class="articles__item--title"><a class="articles_item--title--link" :href="product.product_link" target="blank" v-text="product.title"></a></h3>
+          <div class="articles__item--link"><a :href="product.product_link" target="_blank"><img class="articles__item--link--image" :src="product.img_url" alt="Product image"/></a></div>
           <div class="articles__item--details">
-            <p class="articles__item--details--text" v-text="article.description"></p>
+            <p class="articles__item--details--text" v-text="product.description"></p>
           </div>
         </div>
         <div class="articles__footer">
-          <div class="articles__footer--price">$ @{{ article.price }}</div>
-          <div class="articles__footer--cta"><a class="articles__footer--cta--link" :href="article.buttonLink" role="button">It's a bargain</a></div>
+          <div class="articles__footer--price">$ @{{ product.price }}</div>
+          <div class="articles__footer--cta"><a class="articles__footer--cta--link" :href="product.cta_link" role="button">It's a bargain</a></div>
         </div>
       </div>
     </div>
