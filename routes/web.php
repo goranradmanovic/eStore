2d0df576238e8e2036a93a50dba4206a607241f6
retro12 <goranradmanovic@gmail.com>
2017-02-21 22:23:17 +0100

    Adding new route for app

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,12 @@
 */
 
 //Route for home page
-Route::get('/', 'Controller@index');
+Route::get('/', 'Controller@index')->name('home');
 Route::post('/', 'Controller@createSubscription');
+
+//Route for confirmed email address
+Route::get('/email/confirmed/{id}', 'Controller@confirmedEmail')->name('confirmed');
+
+//API route for getting all data about products in JSON
 Route::get('/products', 'Controller@getAllProducts');
+
