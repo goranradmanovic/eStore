42e62edcbbe573f92a91cbf8810f5e38c747ff71
retro12 <goranradmanovic@gmail.com>
2017-02-21 22:29:40 +0100

    Compailed confirmed blade tempalte

---
+++ resources/views/emails/confirmed.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head></head>
+  <body>
+    <p>Thank you for contacing us and interesting in out newsletter, please confirm your email address by cliking on confirm button. Thank you.</p><a href="{{ route('confirmed', ['id' => $id]) }}">Confirm</a>
+  </body>
+</html>
