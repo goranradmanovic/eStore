44cf445baa6d400ef47f9a6b623ab4573bbb5316
retro12 <goranradmanovic@gmail.com>
2017-02-21 22:22:52 +0100

    Compaile blade template

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -60,7 +60,22 @@
           </div>
         </div>
       </nav>
-    </menu>
+    </menu>@if (Session::has('massageSuccess') || Session::has('messageWarning') || Session::has('messageError'))
+    <div class="row">
+      <div class="col-md-12">
+        <div class="flash">@if (Session::has('massageSuccess'))
+          <div class="flash__message--success">{{ session('massageSuccess') }}</div>
+          @endif
+          @if (Session::has('messageWarning'))
+          <div class="flash__message--warning">{{ session('messageWarning') }}</div>
+          @endif
+          @if (Session::has('messageError'))
+          <div class="flash__message--error">{{ session('messageError') }}
+            <button class="flash__message--error--link">&times;</button>
+          </div>@endif
+        </div>
+      </div>
+    </div>@endif
     <div class="container-fluid" id="app">@yield('content')
       <div class="row">
         <footer class="footer">
