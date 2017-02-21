522f8d9c32a32cbb0bde5eaacbc86bf005b6dbed
retro12 <goranradmanovic@gmail.com>
2017-02-21 22:11:19 +0100

    Creating some usefull functions

--- app/Models/Subscriber/Subscriber.php
+++ app/Models/Subscriber/Subscriber.php
@@ -7,11 +7,40 @@ use Illuminate\Database\Eloquent\Model;
 class Subscriber extends Model
 {
     //Defining fillable for protecting from mass assignable.
-    protected $fillable = ['email'];
+    protected $fillable = [
+    	'email',
+    	'confirmed',
+    ];
 
     //Method for adding new insert into subscribers table
     public function createSusbcribeEmail($email)
     {
-    	
+    	$this->create(['email' => $email]);
+    }
+
+    //Method for getting last subscriber email ID from the subscribers table
+    public function getLastSubscriberEmail()
+    {
+        return $this->latest()->first(['id']);
+    }
+
+    //Method for checking if email address exists in the database table. If email exist return true value in outher case return false value
+    public function isEmailExists($emailId)
+    {
+        return (bool) $this->where('id', $emailId)->first(['id']) ?? true;
+    }
+
+    //Method for cheking if user is already confirm his email, because in that case we just want to redirect him to home page
+    //without updating table again
+    public function isEmailConfirmed($emailId)
+    {
+        //If user is already confirm his email address return true,in other case return false
+        return (bool) $this->where('id', $emailId)->first(['confirmed'])->confirmed ?? true;
+    }
+
+    //Method for updating email confirmed column in subscribe table
+    public function updateEmailConfirmed($emailId)
+    {
+        $this->where('id', $emailId)->update(['confirmed' => 1]);
     }
 }
