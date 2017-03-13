00c2387b0ae93ad90ec8cd2d2665303f18edeef3
retro12 <goranradmanovic@gmail.com>
2017-03-13 21:59:17 +0100

    Getting all category data

--- app/Models/Category/Category.php
+++ app/Models/Category/Category.php
@@ -15,8 +15,15 @@ class Category extends Model
     	return $this->hasOne('App\Models\Product\Product');
     }
 
-    //Method for getting all product items from one category. Arg. is category ID (1 - Tea, 2 - Food Supplement, 3 - Fitness Gear)
-    public function getAllCategoryItems(int $categoryID)
+    //Method for getting id and category_name from category table
+    public function getCategories()
+    {
+        //This will return all id-s and category_names from Category table
+        return $this->get(['id', 'category_name']);
+    }
+
+    //Method for getting all product items from one category. Arg. is category ID (1 - Tea, 2 - Food Supplement, 3 - Fitness Gear, 4 - Random)
+    public function getAllCategoriesItems(int $categoryID)
     {
     	//This will return all product items from one category
     	return $this->find($categoryID)->product()->get();
