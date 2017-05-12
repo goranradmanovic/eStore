0a7f768504a25452928a46ecc82d9f4489cc84fc
retro12 <goranradmanovic@gmail.com>
2017-05-12 23:06:05 +0200

    Compailing new product.blade.php file

--- resources/views/content/product.blade.php
+++ resources/views/content/product.blade.php
@@ -1,2 +1,15 @@
 
-<h1>Hello World!</h1>
+<div class="row">
+  <div class="col-md-12">
+    <div class="product">
+      <div class="col-md-6">
+        <div class="product__image"><span>@{{ products.title }}</span></div>
+      </div>
+      <div class="col-md-6">
+        <div class="product__info">
+          <p>Hello</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
