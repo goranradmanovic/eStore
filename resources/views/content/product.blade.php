8c79058591217362b4acd4297078c4d035c28e2b
retro12 <goranradmanovic@gmail.com>
2017-05-17 22:49:11 +0200

    Compaling blade file

--- resources/views/content/product.blade.php
+++ resources/views/content/product.blade.php
@@ -3,11 +3,15 @@
   <div class="col-md-12">
     <div class="product">
       <div class="col-md-6">
-        <div class="product__image"><span>@{{ products.title }}</span></div>
+        <div class="product__image"><img class="product__image--picture" :src="products.img_url" alt="Product image.jpg"/></div>
       </div>
       <div class="col-md-6">
         <div class="product__info">
-          <p>Hello</p>
+          <h1 class="product__info--title" v-text="products.title"></h1>
+          <div class="product__info--description"><span class="product__info--description--text" v-text="products.description"></span></div>
+          <div class="product__info--lastprice">Last price: <span class="product__info--lastprice--amount">$ @{{ products.price + 11.78 }}</span></div>
+          <div class="product__info--price">Price: <span class="product__info--price--amount">$ @{{ products.price }}</span></div>
+          <div class="product__info--cta"><a class="product__info--cta--link" :href="products.cta_link" role="button">It's a bargain	</a></div>
         </div>
       </div>
     </div>
