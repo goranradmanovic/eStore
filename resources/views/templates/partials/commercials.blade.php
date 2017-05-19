40ef80048a5079644c6d4ad222ac639b38dd7975
retro12 <goranradmanovic@gmail.com>
2017-05-19 23:11:37 +0200

    Compaling new commercials blade file

---
+++ resources/views/templates/partials/commercials.blade.php
@@ -0,0 +1,10 @@
+
+<div class="container-fluid">
+  <div class="row">
+    <div class="col-md-12">
+      <div class="commercials">
+        <div class="commercials__picture"><img class="commercials__picture--image" src="http://placehold.it/1235x110?text=Commercials" alt="Commercials.jpg"/></div>
+      </div>
+    </div>
+  </div>
+</div>
