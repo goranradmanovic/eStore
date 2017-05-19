ccd89a5454c336846ae7698074d731c419b067a8
retro12 <goranradmanovic@gmail.com>
2017-05-19 23:07:48 +0200

    Compaling default blade file

--- resources/views/templates/default.blade.php
+++ resources/views/templates/default.blade.php
@@ -70,6 +70,15 @@
           </div>
         </nav>
       </menu>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="commercials">
+              <div class="commercials__picture"><img class="commercials__picture--image" src="http://placehold.it/1235x110?text=Commercials" alt="Commercials.jpg"></div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div class="container-fluid">@yield('content')
         <div class="row">
           <footer class="footer">
