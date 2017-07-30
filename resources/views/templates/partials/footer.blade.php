2ceb4b876b5bbe886f0e3e81ed860f71d44b5a12
retro12 <goranradmanovic@gmail.com>
2017-07-30 22:19:15 +0200

    New compiled blade files

--- resources/views/templates/partials/footer.blade.php
+++ resources/views/templates/partials/footer.blade.php
@@ -7,7 +7,7 @@
         <div class="footer__social"><a class="footer__social--link footer__social--link--fb" href="#"></a><a class="footer__social--link footer__social--link--tw" href="#"></a><a class="footer__social--link footer__social--link--in" href="#"></a><a class="footer__social--link footer__social--link--li" href="#"></a></div>
       </div>
       <div class="col-md-8">
-        <div class="footer__nav"><a class="footer__nav--link" href="#">Privacy</a><a class="footer__nav--link" href="#">Terms</a><a class="footer__nav--link" href="#">Disclaimer</a><a class="footer__nav--link" href="#">Site Map</a><a class="footer__nav--link" href="#">Contac Us</a></div>
+        <div class="footer__nav"><a class="footer__nav--link" href="#">Privacy</a><a class="footer__nav--link" href="#">Terms</a><a class="footer__nav--link" href="#">Disclaimer</a><a class="footer__nav--link" href="#">Site Map</a><a class="footer__nav--link" href="{{ url('/contact') }}">Contac Us</a></div>
         <div class="footer__form">
           <h3 class="footer__form--title">Subscribe</h3>
           <form class="form-inline footer__form--inline" action="{{ url('/') }}" method="post" autocomplete="off" id="subscribeForm" v-on:submit="submitSubscribeEmail"> 
