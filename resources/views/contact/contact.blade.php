fa2d33909381d20c6ce0e41c2bc6f7a3eef58ed5
retro12 <goranradmanovic@gmail.com>
2017-07-30 22:22:29 +0200

    Creating contact folder

---
+++ resources/views/contact/contact.blade.php
@@ -0,0 +1,15 @@
+
+<div class="row">
+  <div class="col-md-12">
+    <div class="address">
+      <div class="address__info">Piggy Bank</div>
+    </div>
+    <div class="contactform">Contact Form</div>
+  </div>
+  <div class="col-md-12">
+    <div class="share">Share a Project</div>
+  </div>
+  <div class="col-md-12">
+    <div class="map">Map</div>
+  </div>
+</div>
