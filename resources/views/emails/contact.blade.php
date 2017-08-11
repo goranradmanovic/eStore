0deb71866281c2a5d4261d8f7548fe753a8dad31
retro12 <goranradmanovic@gmail.com>
2017-08-11 23:01:52 +0200

    New compiled contact blade file

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+  <head></head>
+  <body>
+    <h2>From: {{ $firstName }} {{ $lastName }}</h2>
+    <h3>Email: {{ $userEmail }}</h3>
+    <p>User Message: <br>{{ $userMessage }}</p>
+  </body>
+</html>
