5c592cda4fc90b4fca7d190258af325774e03f85
retro12 <goranradmanovic@gmail.com>
2017-08-11 22:59:46 +0200

    Creating new migration file

---
+++ database/migrations/2017_08_08_101859_create_contacts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('first_name', 30);
+            $table->string('last_name', 50);
+            $table->string('email', 255);
+            $table->string('message', 255);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
